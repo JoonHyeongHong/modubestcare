85d87a5c996a3b82081679174ad81e38beb52b61
jshwa0429@gmail.com <jshwa0429@gmail.com>
2026-06-26 17:26:19 +0900

    레츠고

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,2 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-// Next.js 표준 설정 사용 (가장 안전하고 호환성이 높음)
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+const eslintConfig = [];
 export default eslintConfig;
