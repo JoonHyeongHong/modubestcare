302703a4391ea156e5b71a899a0c149da01ce8f4
jshwa0429@gmail.com <jshwa0429@gmail.com>
2026-06-26 17:47:12 +0900

    next.config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'modu.local', // 에러에 표시된 외부 도메인 호스트명
+        protocol: 'https',
+        hostname: 'b845-121-173-24-219.ngrok-free.app', // 에러에 표시된 외부 도메인 호스트명
         port: '',
         pathname: '/**',       // 해당 도메인의 모든 경로 이미지 허용
       },
