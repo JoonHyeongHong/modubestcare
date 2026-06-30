eec211e78903a7a641619585dd8c530537117def
jshwa0429@gmail.com <jshwa0429@gmail.com>
2026-06-30 12:55:31 +0900

    ㅎㅎ

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,7 @@ import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import B2BMain from "@/components/home/B2BMain";
 
-import { fetchAPI } from "../lib/api";
 import SideScrollNav from "@/components/common/SideScrollNav";
-const GET_PORTFOLIOS_QUERY = `
-  query GetPortfolios {
-    posts(first: 6, where: { categoryName: "portfolio" }) {
-      nodes {
-        slug    # <- 추가된 부분
-        title 
-        content
-        featuredImage {
-          node {
-            sourceUrl
-          }
-        }
-      }   
-    }
-  }
-`;
 
 // 1. 워드프레스에서 넘어오는 개별 포트폴리오 데이터의 타입을 정의합니다.
 export interface PortfolioNode {
@@ -34,12 +17,6 @@ export interface PortfolioNode {
 }
 
 export default async function Home() {
-  // 1. API 통신으로 워드프레스 데이터 패칭
-  const data = await fetchAPI(GET_PORTFOLIOS_QUERY);
-
-  // 2. 혹시 데이터가 없을 경우를 대비해 빈 배열([]) 처리
-  const portfolios: PortfolioNode[] = data?.posts?.nodes || [];
-
   return (
     <div className="relative w-full">
       <Navbar />
